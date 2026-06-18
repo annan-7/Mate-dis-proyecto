3aa8557a95ead2b7f7e066a8efcafb656db980af
vicente <vicentejavierhuilcaman@gmail.com>
2026-06-18 15:14:36 -0400

    Union con la del jonh

--- interfaz.py
+++ interfaz.py
@@ -61,7 +61,13 @@ def mostrar_grafo(camino):
     
     edges_camino = [(camino[i], camino[i+1]) for i in range(len(camino)-1)]
     nx.draw_networkx_edges(G, pos, edgelist=edges_camino, edge_color='red', width=2)
+    # muestra el origen en verde y el destino en morado
+    nx.draw_networkx_nodes(G, pos, nodelist=[camino[0]], node_color='green', node_size=1000) # nodo de origen en verde
+    nx.draw_networkx_nodes(G, pos, nodelist=[camino[-1]], node_color='purple', node_size=1000) # nodo de destino en morado
     
+    # agrega una leyenda para el origen en verde, rutas, rutas tomadas en rojo y destino en morado
+    plt.legend(["Rutas", "Ruta Tomada", "Ciudad de Origen", "Ciudad de Destino"], loc="upper left", fontsize=12)
+    plt.axis('off') # oculta los ejes
     plt.title("Grafo de Ciudades con Camino Resaltado")
     plt.show()
 
@@ -75,7 +81,7 @@ def mostrar_grafo_completo():
     nx.draw(G, pos, with_labels=True, node_color='lightblue', node_size=1000, font_size=8) # dibuja el grafo con los nodos y las etiquetas
     
     labels = nx.get_edge_attributes(G, 'weight') # obtiene los pesos de las aristas
-    nx.draw_networkx_edge_labels(G, pos, edge_labels=labels) # dibuja los pesos en las aristas
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=labels) # dibuja los pesos en las aristas, "label" es para mostrar el peso de cada arista en el grafo
     
     plt.title("Grafo Completo de Ciudades")
     plt.show()
